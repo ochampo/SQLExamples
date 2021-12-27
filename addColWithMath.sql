069ec3183342dd555509adb874e6a334eecf1228
ochampo <ochampo77@gmail.com>
2021-12-27 13:26:28 -0800

    Organized, by adding folder

--- addColWithMath.sql
+++
@@ -1,6 +0,0 @@
-/****** Script for SelectTopNRows command from SSMS  ******/
-Select Name, ProductNumber, 'adventureWorks' as Manufactures , ListPrice,
-ListPrice * .85 As SalesPrice 
-from Production.Product
-where ListPrice > 0 
-  
